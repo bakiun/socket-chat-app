7be30047153f0d6c9117b2e2b7606bce68f62c32
bakiun <baki94@gmail.com>
2023-03-12 21:41:18 +0300

    new environment keys added

--- API/src/config/index.js
+++ API/src/config/index.js
@@ -3,5 +3,5 @@ const ip = require("ip");
 module.exports = {
   port: process.env.PORT || 3001,
   ip: ip.address(),
-  mongoDBUrl: process.env.MONGO_DB_URL,
+  mongoDBUrl: `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_NAME}`,
 };
