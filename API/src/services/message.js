2205720227f40fdb43696ca2f7568c2874aafc4d
bakiun <baki94@gmail.com>
2023-03-12 17:03:00 +0300

    fix: msg object & variable names

--- API/src/services/message.js
+++ API/src/services/message.js
@@ -11,15 +11,14 @@ exports.oldMsgList = async (from, to) => {
 
 exports.saveMsg = async (from, to, msg) => {
   var user = (await User.findOne({ _id: from.uid })).toObject();
-  if (user[to.uid]) await Message.updateOne({ _id: user[to.uid] }, { [Date.now()]: { username: from.username, msg } }, { strict: false });
+  if (user[to.uid]) await Message.updateOne({ _id: user[to.uid] }, msg, { strict: false });
   else {
     let msgCollection = new Message({});
-    let collectionID = (await msgCollection.save())._id.toString();
-
+    msgCollection = (await msgCollection.save())._id.toString();
     //TODO: Do it with transactions.
-    await User.updateOne({ _id: from.uid }, { [to.uid]: collectionID }, { strict: false });
-    await User.updateOne({ _id: to.uid }, { [from.uid]: collectionID }, { strict: false });
-    await Message.updateOne({ _id: collectionID }, { [Date.now()]: { username: from.username, msg } }, { strict: false });
+    await User.updateOne({ _id: from.uid }, { [to.uid]: msgCollection }, { strict: false });
+    await User.updateOne({ _id: to.uid }, { [from.uid]: msgCollection }, { strict: false });
+    await Message.updateOne({ _id: msgCollection }, msg, { strict: false });
   }
   return;
 };
